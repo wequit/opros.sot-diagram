5f47a42f81e139f7fd13fa7d4a58a40220abb7a1
NPaugust <avgust1ne.k6@gmail.com>
2025-01-24 10:18:07 +0600

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import EvaluationQuestions from '@/app/Evaluations/page';
+import EvaluationQuestions from '@/app/evaluations/page';
 import Data from '@/lib/utils/Data';
 import DataFetcher from '@/components/DataFetcher';
 
