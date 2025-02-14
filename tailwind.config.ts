ba47f2c860490d408f84f261c0faec3c3b76e1d8
NPaugust <avgust1ne.k6@gmail.com>
2025-02-14 20:39:00 +0600

    Деплой 80%

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,7 @@
 import type { Config } from 'tailwindcss';
 
-const config: Config = {
+export default {
   content: [
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@tremor/**/*.{js,ts,jsx,tsx}',
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
@@ -11,59 +9,10 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        primary: {
-          DEFAULT: '#2C3E50',
-          light: '#34495E',
-          dark: '#1A2633'
-        },
-        secondary: {
-          DEFAULT: '#27AE60',
-          light: '#2ECC71',
-          dark: '#219653'
-        },
-        warning: {
-          DEFAULT: '#E67E22',
-          light: '#F39C12',
-          dark: '#D35400'
-        },
-        danger: {
-          DEFAULT: '#E74C3C',
-          light: '#FC6D65',
-          dark: '#C0392B'
-        },
-        neutral: {
-          100: '#F7FAFC',
-          200: '#EDF2F7',
-          300: '#E2E8F0',
-          400: '#CBD5E0',
-          500: '#A0AEC0',
-          600: '#718096',
-          700: '#4A5568',
-          800: '#2D3748',
-          900: '#1A202C'
-        }
-      },
-      tremor: {
-        brand: {
-          faint: "#eff6ff",
-          muted: "#bfdbfe",
-          subtle: "#60a5fa",
-          DEFAULT: "#3b82f6",
-          emphasis: "#1d4ed8",
-          inverted: "#ffffff",
-        },
-      },
-      fontFamily: {
-        inter: ['var(--font-inter)', 'sans-serif'],
+        primary: '#32CD32', // Ваш зеленый цвет для фона
+        secondary: '#F0F0F0', // Серый фон
       },
     },
   },
-  safelist: [
-    {
-      pattern: /^(bg|text|border)-(red|orange|yellow|lime|emerald)-/,
-    }
-  ],
   plugins: [],
-} as const;
-
-export default config;
+} satisfies Config;
